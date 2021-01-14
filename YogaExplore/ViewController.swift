14ce3cbacc746c40aea6f19b093cfca81288cae4
Ankit Batra <me@Ankits-MacBook-Pro-2.local>
2021-01-15 03:01:22 +0530

    UI fixes

--- YogaExplore/ViewController.swift
+++ YogaExplore/ViewController.swift
@@ -14,11 +14,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        navigationController?.delegate = self
     }
 
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         startButton.layer.masksToBounds = true
         startButton.layer.cornerRadius = (startButton.bounds.size.height/2)
         startButton.layer.shadowColor = UIColor(red: (185.0/255.0), green: (160.0/255.0), blue: (137.0/255.0), alpha: 1.0).cgColor
@@ -28,93 +27,4 @@ class ViewController: UIViewController {
         startButton.layer.shadowRadius = 5.0
         startButton.layer.masksToBounds = false
     }
-    
-}
-
-extension ViewController: UIViewControllerTransitioningDelegate, UINavigationControllerDelegate {
-    
-    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return CustomNavigationTransition(originFrame: self.view.frame)
-    }
-}
-
-class CustomNavigationTransition: NSObject, UIViewControllerAnimatedTransitioning {
-
-    let transitionDuration: Double = 5.5
-    private let originFrame: CGRect
-
-    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return TimeInterval(transitionDuration)
-    }
-
-    init(originFrame: CGRect) {
-        self.originFrame = originFrame
-    }
-
-    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        guard let fromVC = transitionContext.viewController(forKey: .from) as? ViewController,
-            let toVC = transitionContext.viewController(forKey: .to) else {
-            transitionContext.completeTransition(true)
-            return
-        }
-        
-        let tabbarController = toVC as? CustomTabBarController
-        if let toVc = (tabbarController?.viewControllers?.first as? UINavigationController)?.viewControllers.first as? YogaViewController {
-            let toView = UIView(frame: toVC.view.frame)
-            toView.addSubview(UIView(frame: CGRect(x: 0, y: 0, width: toVc.view.bounds.size.width, height: 380.0)))
-            if let snapshotView = toVc.view.snapshotView(afterScreenUpdates: false) {
-                toView.addSubview(snapshotView)
-            }
-            let containerView = transitionContext.containerView
-            let startButtonFrame = containerView.convert(
-                fromVC.startButton.frame,
-                from: fromVC.startButton.superview
-            )
-            let startView =  UIView()
-            startView.frame = startButtonFrame
-        
-            // Adding the subviews to the container view.
-            containerView.addSubview(fromVC.view)
-            containerView.addSubview(toVC.view)
-            containerView.addSubview(toView)
-            containerView.addSubview(startView)
-        
-            // Hidding/Showing objects to not/be displayed while animating the transition.
-            fromVC.view.isHidden = false
-            toVC.view.isHidden = true
-            
-
-            // Animate the view objects using PropertyAnimator
-            let animator1 = {
-                UIViewPropertyAnimator(duration: 0.5, dampingRatio: 1) {
-                    startView.transform = CGAffineTransform(scaleX: 0.0, y: 0.0)
-                }
-            }()
-
-            let animator2 = {
-                UIViewPropertyAnimator(duration: 0.5, curve: .easeInOut) {
-                    toView.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
-                }
-            }()
-
-            // Prepare the animations sequence
-            animator1.addCompletion { _ in
-                animator2.startAnimation()
-            }
-
-            animator2.addCompletion { _ in
-                fromVC.view.isHidden = true
-                toVC.view.isHidden = false
-                toView.removeFromSuperview()
-                startView.removeFromSuperview()
-                transitionContext.completeTransition(true)
-            }
-
-            // Run animations
-            animator1.startAnimation()
-        } else {
-            transitionContext.completeTransition(true)
-        }
-    }
-
 }
--- YogaExplore/YogaViewController.swift
+++ YogaExplore/YogaViewController.swift
@@ -13,27 +13,72 @@ class YogaViewController: UIViewController {
     @IBOutlet weak var ratingView: UIView!
     
     @IBOutlet weak var ratingViewheightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var backgroundImageHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var imageContainerView: UIView!
+    @IBOutlet weak var imageBottomContraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leftConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //
         backgroundImageView.layer.cornerRadius = 40
         backgroundImageView.layer.maskedCorners = [.layerMinXMaxYCorner,.layerMaxXMaxYCorner]
         backgroundImageView.layer.masksToBounds = true
-
+        //
         ratingView.layer.masksToBounds = false
         ratingView.layer.shadowColor = UIColor(red: (185.0/255.0), green: (160.0/255.0), blue: (137.0/255.0), alpha: 1.0).cgColor
         ratingView.layer.shadowOffset = CGSize(width: 5, height: 10)
         ratingView.layer.shadowOpacity = 0.2
         ratingView.layer.shadowPath = UIBezierPath(rect: ratingView.bounds).cgPath
         ratingView.layer.shadowRadius = 5.0
+        //
+        let layer = CAGradientLayer()
+        layer.frame = CGRect(x: 0.0, y: 0.0 , width: view.bounds.size.width, height: 83)
+        layer.colors = [UIColor(red: (180.0/255.0), green: (180.0/255.0), blue: (180.0/255.0), alpha:1.0).cgColor, UIColor(red: (205.0/255.0), green: (205.0/255.0), blue: (205.0/255.0), alpha:0.1).cgColor]
+        backgroundImageView.layer.addSublayer(layer)
+        //
+        self.scrollView.alpha = 0
+        self.imageContainerView.alpha = 0
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 0.5) {
+            self.scrollView.alpha = 1
+        }
+        UIView.animate(withDuration: 0.5) {
+            self.imageContainerView.alpha = 1
+        }
+        self.imageBottomContraint.constant = 0
+        self.leadingConstraint.constant = 40
+        self.leftConstraint.constant = 40
+        UIView.animate(withDuration: 0.5, animations: {
+            self.imageContainerView.layoutIfNeeded()
+        }, completion: { (completed) in
+            if completed {
+                self.ratingView.layer.masksToBounds = false
+                self.ratingView.layer.shadowColor = UIColor(red: (185.0/255.0), green: (160.0/255.0), blue: (137.0/255.0), alpha: 1.0).cgColor
+                self.ratingView.layer.shadowOffset = CGSize(width: 5, height: 10)
+                self.ratingView.layer.shadowOpacity = 0.2
+                self.ratingView.layer.shadowPath = UIBezierPath(rect: self.ratingView.bounds).cgPath
+                self.ratingView.layer.shadowRadius = 5.0
+            }
+        })
+    }
     @IBAction func backButtonTapped(_ sender: UIButton) {
-        self.tabBarController?.navigationController?.popToRootViewController(animated: true)
+        self.imageBottomContraint.constant = 250
+        self.leadingConstraint.constant = 70
+        self.leftConstraint.constant = 70
+        UIView.animate(withDuration: 0.3, animations: {
+            self.scrollView.alpha = 0
+            self.imageContainerView.layoutIfNeeded()
+        }, completion: { (completed) in
+            self.tabBarController?.navigationController?.popToRootViewController(animated: false)
+        })
+        
     }
     
     @IBAction func starTapped(_ sender: UIButton) {
@@ -47,15 +92,5 @@ class YogaViewController: UIViewController {
     
     @IBAction func seeOnMapButtonTapped(_ sender: UIButton) {
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
